9d506462d68662b8ab8a761406f8b22c89f71735
Velmora Oils <velmoraoils@gmail.com>
2026-04-25 17:12:46 +0530

    fix routing and CSS linking

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
@@ -41,6 +41,7 @@ export default function App() {
         <RouteFade>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/index.html" element={<Navigate to="/" replace />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/product/:id" element={<ProductDetail />} />
             <Route path="/cart" element={<CartPage />} />
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
   },
-  base: './',
+  base: '/',
 })
